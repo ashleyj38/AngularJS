a1a10f87bf884d0d243a1d5a7ba94c65a0718522
Ashley M. Jones <ashleyj38@gmail.com>
2016-06-01 08:10:47 -0700

    Show star for list item based on boolean value favorite

--- main.ctrl.js
+++ main.ctrl.js
@@ -3,4 +3,28 @@ angular.module('app').controller("MainController", function(){ //Declare control
 
   vm.title = 'AngularJS Tutorial Example'; //Declare a variable title attached to the controller
   vm.searchInput = '';
+
+  //Hash variable
+  vm.shows = [
+    {
+      title: 'Game of Thrones',
+      year: 2011,
+      favorite: true
+    },
+    {
+      title: 'Walking Dead',
+      year: 2010,
+      favorite: false
+    },
+    {
+      title: 'Firefly',
+      year: 2002,
+      favorite: true
+    },
+    {
+      title: 'Banshee',
+      year: 2013,
+      favorite: true
+    }
+  ];
 });
