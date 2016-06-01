0ab513d6cad667554cf8a465abefd0d1d1bfa902
Ashley M. Jones <ashleyj38@gmail.com>
2016-06-01 07:02:44 -0700

    Fixed controller name | Added title variable to controller and referenced in HTML

--- main.ctrl.js
+++ main.ctrl.js
@@ -1,3 +1,6 @@
-angular.module('app').controller("Main Controller", function(){ //Declare controller for app module
+angular.module('app').controller("MainController", function(){ //Declare controller for app module
   var vm = this;
+
+  vm.title = 'AngularJS Tutorial Example'; //Declare a variable title attached to the controller
+
 });
