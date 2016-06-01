61110c38c75a9a9f4411d600bbc0ce6f7f2129d5
Ashley M. Jones <ashleyj38@gmail.com>
2016-06-01 07:19:45 -0700

    Add search bar and bind input

--- main.ctrl.js
+++ main.ctrl.js
@@ -2,5 +2,5 @@ angular.module('app').controller("MainController", function(){ //Declare control
   var vm = this;
 
   vm.title = 'AngularJS Tutorial Example'; //Declare a variable title attached to the controller
-
+  vm.searchInput = '';
 });
